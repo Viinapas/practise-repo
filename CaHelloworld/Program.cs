38fa5afe0aee2279649cd7ff785d858e9ce8a616
Viinapas <pasi.viinanen@gmail.com>
2017-02-14 10:07:07 +0200

    Added Helloworld code

---
+++ CaHelloworld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaHelloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+
+
+            Console.WriteLine("Palauttelemme koodaus oppeja mieleen");
+
+            Console.ReadKey();
+
+        }
+       
+
+    }
+}
